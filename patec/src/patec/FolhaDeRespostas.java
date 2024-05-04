4f0f96f3655a34f1efd2b93a502d5db4a41b67b2
IsabellaOshima <143272475+IsabellaOshima@users.noreply.github.com>
2024-05-04 15:14:44 -0300

    criação da classe FolhaDeResposta e ajuste do GabaritoOficial

---
+++ patec/src/patec/FolhaDeRespostas.java
@@ -0,0 +1,62 @@
+package patec;
+
+public class FolhaDeRespostas {
+	private int codigoRespostas;
+	private char resposta1;
+	private char resposta2;
+	private char resposta3;
+	private char resposta4;
+	private char resposta5;
+	
+	private int codigoGabarito;
+	private String raAluno;
+	
+	public int getCodigoRespostas() {
+		return codigoRespostas;
+	}
+	public void setCodigoRespostas(int codigoRespostas) {
+		this.codigoRespostas = codigoRespostas;
+	}
+	public char getResposta1() {
+		return resposta1;
+	}
+	public void setResposta1(char resposta1) {
+		this.resposta1 = resposta1;
+	}
+	public char getResposta2() {
+		return resposta2;
+	}
+	public void setResposta2(char resposta2) {
+		this.resposta2 = resposta2;
+	}
+	public char getResposta3() {
+		return resposta3;
+	}
+	public void setResposta3(char resposta3) {
+		this.resposta3 = resposta3;
+	}
+	public char getResposta4() {
+		return resposta4;
+	}
+	public void setResposta4(char resposta4) {
+		this.resposta4 = resposta4;
+	}
+	public char getResposta5() {
+		return resposta5;
+	}
+	public void setResposta5(char resposta5) {
+		this.resposta5 = resposta5;
+	}
+	public int getCodigoGabarito() {
+		return codigoGabarito;
+	}
+	public void setCodigoGabarito(int codigoGabarito) {
+		this.codigoGabarito = codigoGabarito;
+	}
+	public String getRaAluno() {
+		return raAluno;
+	}
+	public void setRaAluno(String raAluno) {
+		this.raAluno = raAluno;
+	}
+}
--- patec/src/patec/GabaritoOficial.java
+++ patec/src/patec/GabaritoOficial.java
@@ -2,11 +2,11 @@ package patec;
 
 public class GabaritoOficial {
 	private int codigoGabarito;
-	private int questao1;
-	private int questao2;
-	private int questao3;
-	private int questao4;
-	private int questao5;
+	private char questao1;
+	private char questao2;
+	private char questao3;
+	private char questao4;
+	private char questao5;
 	
 	private String codigoDisciplina;
 	private int codigoAvaliacao;
@@ -17,34 +17,35 @@ public class GabaritoOficial {
 	public void setCodigoGabarito(int codigoGabarito) {
 		this.codigoGabarito = codigoGabarito;
 	}
-	public int getQuestao1() {
+	
+	public char getQuestao1() {
 		return questao1;
 	}
-	public void setQuestao1(int questao1) {
+	public void setQuestao1(char questao1) {
 		this.questao1 = questao1;
 	}
-	public int getQuestao2() {
+	public char getQuestao2() {
 		return questao2;
 	}
-	public void setQuestao2(int questao2) {
+	public void setQuestao2(char questao2) {
 		this.questao2 = questao2;
 	}
-	public int getQuestao3() {
+	public char getQuestao3() {
 		return questao3;
 	}
-	public void setQuestao3(int questao3) {
+	public void setQuestao3(char questao3) {
 		this.questao3 = questao3;
 	}
-	public int getQuestao4() {
+	public char getQuestao4() {
 		return questao4;
 	}
-	public void setQuestao4(int questao4) {
+	public void setQuestao4(char questao4) {
 		this.questao4 = questao4;
 	}
-	public int getQuestao5() {
+	public char getQuestao5() {
 		return questao5;
 	}
-	public void setQuestao5(int questao5) {
+	public void setQuestao5(char questao5) {
 		this.questao5 = questao5;
 	}
 	public String getCodigoDisciplina() {
