d9d820b162e94e2dd6ad0f37a693e3cb90f0ce30
IsabellaOshima <143272475+IsabellaOshima@users.noreply.github.com>
2024-05-04 14:21:12 -0300

    criação da classe Aluno

--- patec/src/patec/Aluno.java
+++ patec/src/patec/Aluno.java
@@ -1,6 +1,35 @@
 package patec;
 
+import java.sql.Date;
+
 public class Aluno {
-	//teste
-	//teste: criando branch
+	
+	private String ra;
+	private String cpf;
+	private String nomeAluno;
+	private Date dataNascimento;
+	public String getRa() {
+		return ra;
+	}
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getNomeAluno() {
+		return nomeAluno;
+	}
+	public void setNomeAluno(String nomeAluno) {
+		this.nomeAluno = nomeAluno;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
 }
