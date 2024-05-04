e14b28a93bd8b72378d2ef3e6d3456ca01d5bce1
IsabellaOshima <143272475+IsabellaOshima@users.noreply.github.com>
2024-05-04 15:17:08 -0300

    criação da classe Avaliacao

---
+++ patec/src/patec/Avaliacao.java
@@ -0,0 +1,32 @@
+package patec;
+
+public class Avaliacao {
+	private int codigoAvaliacao;
+	private int anoAvaliacao;
+	private int semestreAvaliacao;
+	private String tipoAvaliacao;
+	public int getCodigoAvaliacao() {
+		return codigoAvaliacao;
+	}
+	public void setCodigoAvaliacao(int codigoAvaliacao) {
+		this.codigoAvaliacao = codigoAvaliacao;
+	}
+	public int getAnoAvaliacao() {
+		return anoAvaliacao;
+	}
+	public void setAnoAvaliacao(int anoAvaliacao) {
+		this.anoAvaliacao = anoAvaliacao;
+	}
+	public int getSemestreAvaliacao() {
+		return semestreAvaliacao;
+	}
+	public void setSemestreAvaliacao(int semestreAvaliacao) {
+		this.semestreAvaliacao = semestreAvaliacao;
+	}
+	public String getTipoAvaliacao() {
+		return tipoAvaliacao;
+	}
+	public void setTipoAvaliacao(String tipoAvaliacao) {
+		this.tipoAvaliacao = tipoAvaliacao;
+	}
+}
