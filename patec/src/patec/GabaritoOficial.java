14493865ec1c10140af79504c7524696d296037f
IsabellaOshima <143272475+IsabellaOshima@users.noreply.github.com>
2024-05-04 15:07:34 -0300

    criação da classe GabaritoOficial

---
+++ patec/src/patec/GabaritoOficial.java
@@ -0,0 +1,62 @@
+package patec;
+
+public class GabaritoOficial {
+	private int codigoGabarito;
+	private int questao1;
+	private int questao2;
+	private int questao3;
+	private int questao4;
+	private int questao5;
+	
+	private String codigoDisciplina;
+	private int codigoAvaliacao;
+	
+	public int getCodigoGabarito() {
+		return codigoGabarito;
+	}
+	public void setCodigoGabarito(int codigoGabarito) {
+		this.codigoGabarito = codigoGabarito;
+	}
+	public int getQuestao1() {
+		return questao1;
+	}
+	public void setQuestao1(int questao1) {
+		this.questao1 = questao1;
+	}
+	public int getQuestao2() {
+		return questao2;
+	}
+	public void setQuestao2(int questao2) {
+		this.questao2 = questao2;
+	}
+	public int getQuestao3() {
+		return questao3;
+	}
+	public void setQuestao3(int questao3) {
+		this.questao3 = questao3;
+	}
+	public int getQuestao4() {
+		return questao4;
+	}
+	public void setQuestao4(int questao4) {
+		this.questao4 = questao4;
+	}
+	public int getQuestao5() {
+		return questao5;
+	}
+	public void setQuestao5(int questao5) {
+		this.questao5 = questao5;
+	}
+	public String getCodigoDisciplina() {
+		return codigoDisciplina;
+	}
+	public void setCodigoDisciplina(String codigoDisciplina) {
+		this.codigoDisciplina = codigoDisciplina;
+	}
+	public int getCodigoAvaliacao() {
+		return codigoAvaliacao;
+	}
+	public void setCodigoAvaliacao(int codigoAvaliacao) {
+		this.codigoAvaliacao = codigoAvaliacao;
+	}
+}
