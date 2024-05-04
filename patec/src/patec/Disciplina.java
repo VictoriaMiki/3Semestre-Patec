3e5bb141548a40c8c8e47320549201c95be85cff
IsabellaOshima <143272475+IsabellaOshima@users.noreply.github.com>
2024-05-04 15:03:51 -0300

    getters and setters disciplina

--- patec/src/patec/Disciplina.java
+++ patec/src/patec/Disciplina.java
@@ -1,5 +1,25 @@
 package patec;
 
 public class Disciplina {
-	//teste
+	private String codigoDisciplina;
+	private String nomeDisciplina;
+	private int semestreDisciplina;
+	public String getCodigoDisciplina() {
+		return codigoDisciplina;
+	}
+	public void setCodigoDisciplina(String codigoDisciplina) {
+		this.codigoDisciplina = codigoDisciplina;
+	}
+	public String getNomeDisciplina() {
+		return nomeDisciplina;
+	}
+	public void setNomeDisciplina(String nomeDisciplina) {
+		this.nomeDisciplina = nomeDisciplina;
+	}
+	public int getSemestreDisciplina() {
+		return semestreDisciplina;
+	}
+	public void setSemestreDisciplina(int semestreDisciplina) {
+		this.semestreDisciplina = semestreDisciplina;
+	}
 }
