b1143b30a047573cd89fc0e4b217019b0855a3a1
Matheus Silva <matpaixaoo@outlook.com>
2024-05-06 14:32:13 -0300

    feat(view): delimitando as classes para a criação das telas.

---
+++ patec/src/view/PainelLogin.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.JPanel;
+
+public class PainelLogin extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public PainelLogin() {
+
+	}
+
+}
