cf4ba0a4f6976e9ab624607bec56cbb792129c14
Matheus Silva <matpaixaoo@outlook.com>
2024-05-08 13:24:39 -0300

    feat(menu coordenador): adição de elementos básicos do menu do coord.
    Adição de uma menuBar, menu para visualizar as opções de cadastros, menu para visualizar as opções de relatórios e um botão para finalizar a sessão.

--- patec/src/view/PainelMenuCoordenador.java
+++ patec/src/view/PainelMenuCoordenador.java
@@ -1,6 +1,7 @@
 package view;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class PainelMenuCoordenador extends JPanel {
 
@@ -10,7 +11,48 @@ public class PainelMenuCoordenador extends JPanel {
 	 * Create the panel.
 	 */
 	public PainelMenuCoordenador() {
-
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{450, 0};
+		gridBagLayout.rowHeights = new int[]{28, 272, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		JMenuBar menuBar = new JMenuBar();
+		GridBagConstraints gbc_menuBar = new GridBagConstraints();
+		gbc_menuBar.anchor = GridBagConstraints.NORTH;
+		gbc_menuBar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_menuBar.insets = new Insets(0, 0, 5, 0);
+		gbc_menuBar.gridx = 0;
+		gbc_menuBar.gridy = 0;
+		add(menuBar, gbc_menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Cadastro");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Gabarito");
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Aluno");
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Disciplina");
+		mnNewMenu.add(mntmNewMenuItem_2);
+		
+		JMenu mnNewMenu_1 = new JMenu("Relatório");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Por Disciplina");
+		mnNewMenu_1.add(mntmNewMenuItem_3);
+		
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Por Aluno");
+		mnNewMenu_1.add(mntmNewMenuItem_4);
+		
+		JButton btnNewButton = new JButton("Sair");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.anchor = GridBagConstraints.NORTHEAST;
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 1;
+		add(btnNewButton, gbc_btnNewButton);
 	}
 
 }
