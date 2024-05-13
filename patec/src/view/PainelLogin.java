2523477f781f6eddbb3270769eef7aaa9b3d80bc
Matheus Silva <matpaixaoo@outlook.com>
2024-05-13 18:36:50 -0300

    feat(login): implementação parcial do controle de usuários.

--- patec/src/view/PainelLogin.java
+++ patec/src/view/PainelLogin.java
@@ -3,7 +3,6 @@ package view;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.GroupLayout.*;
 
 public class PainelLogin extends JPanel {
 
@@ -22,79 +21,83 @@ public class PainelLogin extends JPanel {
 		setLayout(gridBagLayout);
 		
 		loginContainer = new JPanel();
-		
-		JLabel lblUsuario = new JLabel("New label");
-		
-		JLabel lblSenha = new JLabel("New label");
-		
-		JLabel lblLogin = new JLabel("New label");
-		
-		tfUsuario = new JTextField();
-		
-		tfSenha = new JTextField();
-		
-		JButton btnEntrar = new JButton("New button");
-		btnEntrar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				PainelMenuAluno p = new PainelMenuAluno();
-				FramePatec.frame.setContentPane(p);
-				FramePatec.frame.revalidate();
-				FramePatec.frame.repaint();
-			}
-		});
-		
-		GroupLayout gl_loginContainer = new GroupLayout(loginContainer);
-		gl_loginContainer.setHorizontalGroup(
-			gl_loginContainer.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_loginContainer.createSequentialGroup()
-					.addContainerGap(26, Short.MAX_VALUE)
-					.addGroup(gl_loginContainer.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_loginContainer.createSequentialGroup()
-							.addGap(80)
-							.addComponent(lblLogin))
-						.addGroup(gl_loginContainer.createSequentialGroup()
-							.addComponent(lblUsuario)
-							.addGap(4)
-							.addComponent(tfUsuario, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_loginContainer.createSequentialGroup()
-							.addComponent(lblSenha)
-							.addGap(4)
-							.addComponent(tfSenha, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE))
-						.addGroup(gl_loginContainer.createSequentialGroup()
-							.addGap(58)
-							.addComponent(btnEntrar)))
-					.addGap(24))
-		);
-		
-		gl_loginContainer.setVerticalGroup(
-			gl_loginContainer.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_loginContainer.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblLogin)
-					.addGap(6)
-					.addGroup(gl_loginContainer.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_loginContainer.createSequentialGroup()
-							.addGap(3)
-							.addComponent(lblUsuario))
-						.addComponent(tfUsuario, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(6)
-					.addGroup(gl_loginContainer.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_loginContainer.createSequentialGroup()
-							.addGap(3)
-							.addComponent(lblSenha))
-						.addComponent(tfSenha, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(6)
-					.addComponent(btnEntrar)
-					.addContainerGap(17, Short.MAX_VALUE))
-		);
-		
-		loginContainer.setLayout(gl_loginContainer);
 		GridBagConstraints gbc_loginContainer = new GridBagConstraints();
-		gbc_loginContainer.anchor = GridBagConstraints.NORTH;
-		gbc_loginContainer.fill = GridBagConstraints.HORIZONTAL;
+		gbc_loginContainer.fill = GridBagConstraints.BOTH;
 		gbc_loginContainer.gridx = 1;
 		gbc_loginContainer.gridy = 1;
 		add(loginContainer, gbc_loginContainer);
+		GridBagLayout gbl_loginContainer = new GridBagLayout();
+		gbl_loginContainer.columnWidths = new int[]{0, 2, 106, 0, 0};
+		gbl_loginContainer.rowHeights = new int[]{0, 12, 20, 20, 23, 0, 0};
+		gbl_loginContainer.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_loginContainer.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		loginContainer.setLayout(gbl_loginContainer);
+		
+		JLabel lblLogin = new JLabel("Log-in");
+		GridBagConstraints gbc_lblLogin = new GridBagConstraints();
+		gbc_lblLogin.anchor = GridBagConstraints.SOUTH;
+		gbc_lblLogin.insets = new Insets(0, 0, 5, 5);
+		gbc_lblLogin.gridx = 2;
+		gbc_lblLogin.gridy = 1;
+		loginContainer.add(lblLogin, gbc_lblLogin);
+		
+		JLabel lblUsuario = new JLabel("Usuário:");
+		GridBagConstraints gbc_lblUsuario = new GridBagConstraints();
+		gbc_lblUsuario.fill = GridBagConstraints.VERTICAL;
+		gbc_lblUsuario.anchor = GridBagConstraints.EAST;
+		gbc_lblUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUsuario.gridx = 1;
+		gbc_lblUsuario.gridy = 2;
+		loginContainer.add(lblUsuario, gbc_lblUsuario);
+		
+		tfUsuario = new JTextField();
+		GridBagConstraints gbc_tfUsuario = new GridBagConstraints();
+		gbc_tfUsuario.fill = GridBagConstraints.BOTH;
+		gbc_tfUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_tfUsuario.gridx = 2;
+		gbc_tfUsuario.gridy = 2;
+		loginContainer.add(tfUsuario, gbc_tfUsuario);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (tfUsuario.getText().contentEquals("coordenador") && !(tfSenha.getText().isBlank())) {
+					PainelMenuCoordenador p = new PainelMenuCoordenador();
+					FramePatec.frame.setContentPane(p);
+					FramePatec.frame.revalidate();
+					FramePatec.frame.repaint();
+				} else if (tfUsuario.getText().contentEquals("aluno") && !(tfSenha.getText().isBlank())){
+					PainelMenuAluno p = new PainelMenuAluno();
+					FramePatec.frame.setContentPane(p);
+					FramePatec.frame.revalidate();
+					FramePatec.frame.repaint();
+				}
+			}
+		});
+		
+		JLabel lblSenha = new JLabel("Senha:");
+		GridBagConstraints gbc_lblSenha = new GridBagConstraints();
+		gbc_lblSenha.fill = GridBagConstraints.VERTICAL;
+		gbc_lblSenha.anchor = GridBagConstraints.EAST;
+		gbc_lblSenha.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSenha.gridx = 1;
+		gbc_lblSenha.gridy = 3;
+		loginContainer.add(lblSenha, gbc_lblSenha);
+		
+		tfSenha = new JTextField();
+		GridBagConstraints gbc_tfSenha = new GridBagConstraints();
+		gbc_tfSenha.fill = GridBagConstraints.BOTH;
+		gbc_tfSenha.insets = new Insets(0, 0, 5, 5);
+		gbc_tfSenha.gridx = 2;
+		gbc_tfSenha.gridy = 3;
+		loginContainer.add(tfSenha, gbc_tfSenha);
+		
+		GridBagConstraints gbc_btnEntrar = new GridBagConstraints();
+		gbc_btnEntrar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnEntrar.anchor = GridBagConstraints.NORTH;
+		gbc_btnEntrar.gridx = 2;
+		gbc_btnEntrar.gridy = 4;
+		loginContainer.add(btnEntrar, gbc_btnEntrar);
 
 	}
 }
