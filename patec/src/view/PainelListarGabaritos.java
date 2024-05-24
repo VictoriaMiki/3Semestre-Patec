4125a6e3f5c08796e6b8ec2115403be38fdce74a
Matheus Silva <matpaixaoo@outlook.com>
2024-05-24 19:08:40 -0300

    feat: configuração inicial da tabela.
    Houve também a substituição da parte do código relacionada ao menu bar.

--- patec/src/view/PainelListarGabaritos.java
+++ patec/src/view/PainelListarGabaritos.java
@@ -4,9 +4,15 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import view.resources.BaseTable;
+import view.resources.MenuBarCoord;
+
 public class PainelListarGabaritos extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JTable tabelaGabaritos;
+	private String[] colunas = {"Código", "Questão 1", "Questão 2", "Questão 3", "Questão 4", "Questão 5", "Disciplina", "Avaliação"};
+	private Object[][] dados = {{null, null, null, null, null, null, null, null}};
 
 	/**
 	 * Create the panel.
@@ -14,50 +20,20 @@ public class PainelListarGabaritos extends JPanel {
 	public PainelListarGabaritos() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[] { 0, 450, 0, 0 };
-		gridBagLayout.rowHeights = new int[] { 28, 272, 0 };
+		gridBagLayout.rowHeights = new int[] { 28, 0, 272, 0, 0 };
 		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, 1.0, 1.0, Double.MIN_VALUE };
 		setLayout(gridBagLayout);
-		JMenuBar menuBar = new JMenuBar();
-		GridBagConstraints gbc_menuBar = new GridBagConstraints();
-		gbc_menuBar.gridwidth = 3;
-		gbc_menuBar.anchor = GridBagConstraints.NORTH;
-		gbc_menuBar.fill = GridBagConstraints.HORIZONTAL;
-		gbc_menuBar.insets = new Insets(0, 0, 5, 0);
-		gbc_menuBar.gridx = 0;
-		gbc_menuBar.gridy = 0;
-		add(menuBar, gbc_menuBar);
-
-		JMenu mnNewMenu = new JMenu("Cadastro");
-		menuBar.add(mnNewMenu);
-
-		JMenuItem mntmNewMenuItem = new JMenuItem("Gabarito");
-		mnNewMenu.add(mntmNewMenuItem);
-
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Aluno");
-		mnNewMenu.add(mntmNewMenuItem_1);
-
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Disciplina");
-		mnNewMenu.add(mntmNewMenuItem_2);
-
-		JMenu mnNewMenu_1 = new JMenu("Relatório");
-		menuBar.add(mnNewMenu_1);
-
-		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Por Disciplina");
-		mnNewMenu_1.add(mntmNewMenuItem_3);
-
-		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Por Aluno");
-		mnNewMenu_1.add(mntmNewMenuItem_4);
-
-		JButton btnSair = new JButton("Sair");
-		btnSair.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				PainelLogin p = new PainelLogin();
-				FramePatec.frame.setContentPane(p);
-				FramePatec.frame.revalidate();
-				FramePatec.frame.repaint();
-			}
-		});
+		
+		MenuBarCoord mbc = new MenuBarCoord();
+		GridBagConstraints gbc_mbc = new GridBagConstraints();
+		gbc_mbc.gridwidth = 3;
+		gbc_mbc.anchor = GridBagConstraints.NORTH;
+		gbc_mbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc_mbc.insets = new Insets(0, 0, 5, 0);
+		gbc_mbc.gridx = 0;
+		gbc_mbc.gridy = 0;
+		add(mbc, gbc_mbc);
 
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.addActionListener(new ActionListener() {
@@ -69,16 +45,39 @@ public class PainelListarGabaritos extends JPanel {
 			}
 		});
 		GridBagConstraints gbc_btnVoltar = new GridBagConstraints();
-		gbc_btnVoltar.anchor = GridBagConstraints.NORTH;
-		gbc_btnVoltar.insets = new Insets(0, 0, 0, 5);
+		gbc_btnVoltar.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnVoltar.insets = new Insets(0, 0, 5, 5);
 		gbc_btnVoltar.gridx = 0;
 		gbc_btnVoltar.gridy = 1;
 		add(btnVoltar, gbc_btnVoltar);
+
+		JButton btnSair = new JButton("Sair");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PainelLogin p = new PainelLogin();
+				FramePatec.frame.setContentPane(p);
+				FramePatec.frame.revalidate();
+				FramePatec.frame.repaint();
+			}
+		});
 		GridBagConstraints gbc_btnSair = new GridBagConstraints();
+		gbc_btnSair.insets = new Insets(0, 0, 5, 0);
 		gbc_btnSair.anchor = GridBagConstraints.NORTHEAST;
 		gbc_btnSair.gridx = 2;
 		gbc_btnSair.gridy = 1;
 		add(btnSair, gbc_btnSair);
-	}
 
+		JScrollPane containerListaGabaritos = new JScrollPane();
+		GridBagConstraints gbc_containerListaGabaritos = new GridBagConstraints();
+		gbc_containerListaGabaritos.insets = new Insets(0, 0, 5, 5);
+		gbc_containerListaGabaritos.fill = GridBagConstraints.BOTH;
+		gbc_containerListaGabaritos.gridx = 1;
+		gbc_containerListaGabaritos.gridy = 2;
+		add(containerListaGabaritos, gbc_containerListaGabaritos);
+		
+		tabelaGabaritos = new JTable(new BaseTable(colunas, dados));
+		tabelaGabaritos.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		containerListaGabaritos.setViewportView(tabelaGabaritos);
+		tabelaGabaritos.setFillsViewportHeight(true);
+	}
 }
