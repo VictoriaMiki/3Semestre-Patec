f81fa9f57ed4be15ac6eec141adb9542b73b16e7
IsabellaOshima <143272475+IsabellaOshima@users.noreply.github.com>
2024-06-02 11:49:50 -0300

    Criando página de lista das avaliações

--- patec/src/view/PainelListarAvaliacoes.java
+++ patec/src/view/PainelListarAvaliacoes.java
@@ -1,16 +1,155 @@
 package view;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+
+import model.AvaliacaoDAO;
+import model.BD;
+import view.resources.BtnSair;
+import view.resources.BtnVoltar;
+import view.resources.MenuBarCoord;
+import view.resources.TableModelPatec;
 
 public class PainelListarAvaliacoes extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-
+	private JTable tabelaAvaliacoes;
+	private DefaultTableModel model;
+	private BD bd;
+	private AvaliacaoDAO dao = new AvaliacaoDAO();
+	
 	/**
 	 * Create the panel.
 	 */
 	public PainelListarAvaliacoes() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 66, 450, 100, 66, 0 };
+		gridBagLayout.rowHeights = new int[] { 28, 0, 0, 272, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 1.0, 1.0, 1.0, Double.MIN_VALUE };
+		setLayout(gridBagLayout);
 
+		MenuBarCoord mbc = new MenuBarCoord();
+		GridBagConstraints gbc_mbc = new GridBagConstraints();
+		gbc_mbc.gridwidth = 4;
+		gbc_mbc.anchor = GridBagConstraints.NORTH;
+		gbc_mbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc_mbc.insets = new Insets(0, 0, 5, 0);
+		gbc_mbc.gridx = 0;
+		gbc_mbc.gridy = 0;
+		add(mbc, gbc_mbc);
+
+		BtnVoltar btnVoltar = new BtnVoltar("Voltar");
+		GridBagConstraints gbc_btnVoltar = new GridBagConstraints();
+		gbc_btnVoltar.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btnVoltar.insets = new Insets(0, 0, 5, 5);
+		gbc_btnVoltar.gridx = 0;
+		gbc_btnVoltar.gridy = 1;
+		add(btnVoltar, gbc_btnVoltar);
+
+		BtnSair btnSair = new BtnSair("Sair");
+		GridBagConstraints gbc_btnSair = new GridBagConstraints();
+		gbc_btnSair.insets = new Insets(0, 0, 5, 0);
+		gbc_btnSair.gridx = 3;
+		gbc_btnSair.gridy = 1;
+		add(btnSair, gbc_btnSair);
+
+		JScrollPane containerListaAvaliacoes = new JScrollPane();
+		GridBagConstraints gbc_containerListaAvaliacoes = new GridBagConstraints();
+		gbc_containerListaAvaliacoes.insets = new Insets(0, 0, 5, 5);
+		gbc_containerListaAvaliacoes.fill = GridBagConstraints.BOTH;
+		gbc_containerListaAvaliacoes.gridx = 1;
+		gbc_containerListaAvaliacoes.gridy = 3;
+		add(containerListaAvaliacoes, gbc_containerListaAvaliacoes);
+
+		tabelaAvaliacoes = new JTable();
+		bd = new BD();
+		if (bd.getConnection()) {
+			carregarTabela();
+		} else {
+			JOptionPane.showMessageDialog(null, "Falha na Conexão");
+			PainelMenuCoordenador p = new PainelMenuCoordenador();
+			FramePatec.getFrame().setContentPane(p);
+			FramePatec.getFrame().revalidate();
+			FramePatec.getFrame().repaint();
+		}
+		tabelaAvaliacoes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		containerListaAvaliacoes.setViewportView(tabelaAvaliacoes);
+		tabelaAvaliacoes.setFillsViewportHeight(true);
+
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.insets = new Insets(0, 0, 5, 5);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 2;
+		gbc_panel.gridy = 3;
+		add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] { 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0, 0, 0, 0 };
+		gbl_panel.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		panel.setLayout(gbl_panel);
+
+		JButton btnNewButton = new JButton("Cadastrar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PainelCadastroAvaliacao p = new PainelCadastroAvaliacao();
+				FramePatec.getFrame().setContentPane(p);
+				FramePatec.getFrame().revalidate();
+				FramePatec.getFrame().repaint();
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.fill = GridBagConstraints.BOTH;
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 1;
+		panel.add(btnNewButton, gbc_btnNewButton);
+
+		JButton btnNewButton_1 = new JButton("Editar");
+		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
+		gbc_btnNewButton_1.fill = GridBagConstraints.BOTH;
+		gbc_btnNewButton_1.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton_1.gridx = 0;
+		gbc_btnNewButton_1.gridy = 2;
+		panel.add(btnNewButton_1, gbc_btnNewButton_1);
+
+		JButton btnNewButton_2 = new JButton("Excluir");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(tabelaAvaliacoes.getSelectedRow() != -1 && JOptionPane.showConfirmDialog(null,
+						"Deseja excluir este registro?", "Confirmar exclusão", JOptionPane.YES_NO_OPTION) == 0) {
+					dao.excluir(tabelaAvaliacoes.getValueAt(tabelaAvaliacoes.getSelectedRow(),  0));
+					PainelListarAvaliacoes p = new PainelListarAvaliacoes();
+					FramePatec.getFrame().setContentPane(p);
+					FramePatec.getFrame().revalidate();
+					FramePatec.getFrame().repaint();
+				}
+			}
+		});
+		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
+		gbc_btnNewButton_2.fill = GridBagConstraints.BOTH;
+		gbc_btnNewButton_2.gridx = 0;
+		gbc_btnNewButton_2.gridy = 3;
+		panel.add(btnNewButton_2, gbc_btnNewButton_2);
+	}
+
+	private void carregarTabela() {
+		String sql = "SELECT * FROM AVALIACAO";
+		model = TableModelPatec.getModel(bd, sql);
+		tabelaAvaliacoes.setModel(model);
 	}
 
 }
