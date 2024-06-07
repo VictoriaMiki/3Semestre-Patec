6e3d37ea2011082908dc7dd26635fae11494c14f
Matheus Silva <matpaixaoo@outlook.com>
2024-06-07 10:16:26 -0300

    docs(Disciplina): Documentação das classes de negócio.

--- patec/src/model/Disciplina.java
+++ patec/src/model/Disciplina.java
@@ -6,36 +6,83 @@ public class Disciplina {
 	private String nomeDisciplina;
 	private int semestreDisciplina;
 
+	/**
+	 * Cria uma nova instância de <code>Disciplina</code>, sem nenhum valor inicial
+	 * atribuído aos atributos da instância.
+	 */
 	public Disciplina() {
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Cria uma nova instância de <code>Disciplina</code>, com os valores iniciais
+	 * atribuídos aos atributos da instância.
+	 * 
+	 * @param codigoDisciplina   - uma <code>String</code> que corresponde ao código
+	 *                           da disciplina.
+	 * @param nomeDisciplina     - uma <code>String</code> que corresponde ao nome
+	 *                           da disciplina.
+	 * @param semestreDisciplina - um valor do tipo <code>int</code>, que
+	 *                           corresponde ao semestre da disciplina.
+	 */
 	public Disciplina(String codigoDisciplina, String nomeDisciplina, int semestreDisciplina) {
 		this.codigoDisciplina = codigoDisciplina;
 		this.nomeDisciplina = nomeDisciplina;
 		this.semestreDisciplina = semestreDisciplina;
 	}
 
+	/**
+	 * Recupera o valor atribuído ao atributo <code>codigoDisciplina</code>.
+	 * 
+	 * @return O valor do atributo <code>codigoDisciplina</code>.
+	 */
 	public String getCodigoDisciplina() {
 		return codigoDisciplina;
 	}
 
+	/**
+	 * Define o valor do atributo <code>codigoDisciplina</code>.
+	 * 
+	 * @param codigoDisciplina - uma <code>String</code> que corresponde ao código
+	 *                         da disciplina.
+	 */
 	public void setCodigoDisciplina(String codigoDisciplina) {
 		this.codigoDisciplina = codigoDisciplina;
 	}
 
+	/**
+	 * Recupera o valor atribuído ao atributo <code>nomeDisciplina</code>.
+	 * 
+	 * @return O valor do atributo <code>nomeDisciplina</code>.
+	 */
 	public String getNomeDisciplina() {
 		return nomeDisciplina;
 	}
 
+	/**
+	 * Define o valor do atributo <code>nomeDisciplina</code>.
+	 * 
+	 * @param nomeDisciplina - uma <code>String</code> que corresponde ao nome da
+	 *                       disciplina.
+	 */
 	public void setNomeDisciplina(String nomeDisciplina) {
 		this.nomeDisciplina = nomeDisciplina;
 	}
 
+	/**
+	 * Recupera o valor atribuído ao atributo <code>semestreDisciplina</code>.
+	 * 
+	 * @return O valor do atributo <code>semestreDisciplina</code>.
+	 */
 	public int getSemestreDisciplina() {
 		return semestreDisciplina;
 	}
 
+	/**
+	 * Define o valor do atributo <code>semestreDisciplina</code>.
+	 * 
+	 * @param semestreDisciplina - um valor do tipo <code>int</code>, que
+	 *                           corresponde ao semestre da disciplina.
+	 */
 	public void setSemestreDisciplina(int semestreDisciplina) {
 		this.semestreDisciplina = semestreDisciplina;
 	}
